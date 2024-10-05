1ec7b4c62c7266f65c493c394fd2c11c1bd2cd3d
codericky <thevenom425@gmail.com>
2024-10-05 15:05:09 +0700

    add repository

---
+++ Learning/main3.py
@@ -0,0 +1,119 @@
+# Mendefinisikan variabel
+angka1 = 10
+angka2 = 5
+
+# # Operasi penjumlahan
+# hasil_penjumlahan = angka1 + angka2
+# print("Hasil penjumlahan:", hasil_penjumlahan)
+
+# # Operasi pengurangan
+# hasil_pengurangan = angka1 - angka2
+# print("Hasil pengurangan:", hasil_pengurangan)
+
+# # Operasi perkalian
+# hasil_perkalian = angka1 * angka2
+# print("Hasil perkalian:", hasil_perkalian)
+
+# # Operasi pembagian
+# hasil_pembagian = angka1 / angka2
+# print("Hasil pembagian:", hasil_pembagian)
+
+# # Operasi pemangkatan
+# hasil_pemangkatan = angka1 ** angka2
+# print("Hasil pemangkatan:", hasil_pemangkatan)
+
+print(angka1 == angka2)
+print(angka1 != angka2)
+print(angka1 > angka2)
+print(angka1 < angka2)
+print(angka1 >= angka2)
+print(angka1 <= angka1)
+
+
+
+
+# PERCABANGAN IF
+# angka = 5
+
+# if angka > 0:
+#     print("Angka positif")
+# elif angka < 0:
+#     print("Angka negatif")
+# else:
+#     print("Angka nol")
+
+
+#PERCABANGAN ELIF
+# nilai = int(input("Masukkan nilai ujian: "))
+
+# if nilai >= 90:
+#   print("Nilai Anda A")
+# elif nilai >= 80:
+#   print("Nilai Anda B")
+# elif nilai >= 70:
+#   print("Nilai Anda C")
+# else:
+#   print("Nilai Anda kurang dari C")
+
+
+# PERULANGAN FOR
+# for i in range(5):
+#      print(i)    
+
+# for i in range(1, 11):
+#     if i == 5:
+#         break
+#     print(i)
+
+# PERULANGAN WHILE
+# angka = 1
+# while angka <= 20:
+#     print(angka)
+#     angka += 1
+
+# BREAK AND CONTINOUE
+# angka = [1, 2, 3, -1, 5, 6, 7]
+# target = 8
+# found = False
+
+# for angka_ini in angka:
+#     if angka_ini == target:
+#         print(f"Angka {target} ditemukan!")
+#         found = True
+#         break  # Hentikan perulangan jika angka ditemukan
+
+#     if angka_ini < 0:
+#         continue  # Lewati angka negatif
+
+# if not found:
+#     print("Angka yang dicari tidak ditemukan.")
+
+
+# OPERATOR LOGIKA AND
+# angka1 = 10
+# angka2 = 30
+
+# if angka1 > 5 and angka2 > 5:
+#     print("Kedua angka lebih besar dari 5.")
+# else:
+#     print("Salah satu atau kedua angka tidak lebih besar dari 5.")
+
+
+# OPEARTOR LOGIKA OR
+# angka1 = 10
+# angka2 = 30
+
+# if angka1 == 10 or angka2 == 10:
+#     print("Salah satu angka sama dengan 10.")
+# else:
+#     print("Tidak ada angka yang sama dengan 10.")
+
+
+# OPERATOR LOGIKA NOT
+# angka1 = 10
+# angka2 = 30
+
+# if not (angka1 == angka2):
+#     print("Kedua angka tidak sama.")
+# else:
+#     print("Kedua angka sama.")    
